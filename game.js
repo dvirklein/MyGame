5482ebe85d62eef137e436c86ecc67853ca2994d
dvirklein <77276010+dvirklein@users.noreply.github.com>
2025-03-24 14:15:43 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,5 +1,3 @@
-// Updated Phaser game with improved UI and continuous shooting
-
 class StartScene extends Phaser.Scene {
     constructor() {
         super({ key: 'StartScene' });
@@ -12,7 +10,7 @@ class StartScene extends Phaser.Scene {
     create() {
         let startButton = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'startButton')
             .setInteractive()
-            .setScale(1.2);
+            .setScale(0.5);
 
         startButton.on('pointerdown', () => {
             this.scene.start('GameScene');
@@ -28,43 +26,37 @@ class GameScene extends Phaser.Scene {
     preload() {
         this.load.image('player', 'assets/player.png');
         this.load.image('bullet', 'assets/bullet.png');
-        this.load.image('enemy', 'assets/enemy.png');
-        this.load.image('powerup', 'assets/powerup.png');
         this.load.image('mushroom', 'assets/mushroom.png');
+        this.load.image('powerup', 'assets/powerup.png');
     }
 
     create() {
-        this.player = this.physics.add.sprite(200, 500, 'player').setScale(0.7);
+        this.player = this.physics.add.sprite(400, 500, 'player').setScale(0.7);
         this.cursors = this.input.keyboard.createCursorKeys();
-        this.shootButton = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-        this.shooting = false;
+        this.spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        this.isShooting = false;
 
-        this.shootButton.on('down', () => {
-            this.shooting = true;
-            this.shootBullet();
-        });
-        
-        this.shootButton.on('up', () => {
-            this.shooting = false;
+        this.bullets = this.physics.add.group({
+            defaultKey: 'bullet',
+            maxSize: 10
         });
 
-        this.time.addEvent({
-            delay: 200,
-            loop: true,
-            callback: () => {
-                if (this.shooting) {
-                    this.shootBullet();
-                }
-            }
+        this.mushrooms = this.physics.add.group();
+        for (let i = 0; i < 5; i++) {
+            this.mushrooms.create(Phaser.Math.Between(100, 700), Phaser.Math.Between(50, 300), 'mushroom').setScale(0.6);
+        }
+
+        this.powerups = this.physics.add.group();
+        for (let i = 0; i < 3; i++) {
+            this.powerups.create(Phaser.Math.Between(100, 700), Phaser.Math.Between(100, 400), 'powerup').setScale(0.4);
+        }
+
+        this.shootTimer = this.time.addEvent({
+            delay: 300,
+            callback: this.shootBullet,
+            callbackScope: this,
+            loop: true
         });
-
-        this.mushroom = this.physics.add.sprite(300, 400, 'mushroom').setScale(1.1);
-        this.powerup = this.physics.add.sprite(100, 200, 'powerup').setScale(0.8);
-    }
-
-    shootBullet() {
-        let bullet = this.physics.add.sprite(this.player.x, this.player.y - 20, 'bullet');
-        bullet.setVelocityY(-300);
     }
 
     update() {
@@ -75,16 +67,38 @@ class GameScene extends Phaser.Scene {
         } else {
             this.player.setVelocityX(0);
         }
+
+        if (this.spaceKey.isDown) {
+            if (!this.isShooting) {
+                this.isShooting = true;
+                this.shootBullet();
+            }
+        } else {
+            this.isShooting = false;
+        }
+    }
+
+    shootBullet() {
+        if (!this.isShooting) return;
+
+        let bullet = this.bullets.get(this.player.x, this.player.y - 20);
+        if (bullet) {
+            bullet.setActive(true);
+            bullet.setVisible(true);
+            bullet.setVelocityY(-300);
+        }
     }
 }
 
 const config = {
     type: Phaser.AUTO,
-    width: window.innerWidth,
-    height: window.innerHeight,
+    width: 800,
+    height: 600,
     physics: {
         default: 'arcade',
-        arcade: { debug: false }
+        arcade: {
+            debug: false
+        }
     },
     scene: [StartScene, GameScene]
 };
